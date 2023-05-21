3a670f8599312a61789affaafacd034d0c4b8a0f
jordan-75 <74538110+jordan-75@users.noreply.github.com>
2023-05-21 23:18:00 +0200

    loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,17 @@
-import { Suspense } from 'react';
-import { useRouter } from 'next/router';
-import dynamic from 'next/dynamic';
-
-const Navbar = dynamic(() => import('/components/Navbar'));
-const JohnnyBravoComponent = dynamic(() => import('/components/JohnnyBravoComponent'));
-const About = dynamic(() => import('/components/About'));
-const Footer = dynamic(() => import('/components/Footer'));
+import Navbar from '/components/NavBar'
+import FanClub from '/components/FanClub'
+import JohnnyBravoComponent from '/components/JohnnyBravoComponent';  
+import About from '/components/About';  
+import Footer from '/components/Footer';  
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <>
       <Navbar/> 
-      <Suspense fallback={<div>Loading...</div>}>
+       {/* <FanClub/>  */}
         <JohnnyBravoComponent />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <About />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Footer />
-      </Suspense>
+         <About />
+         <Footer />
     </>
-  );
+  )
 }
